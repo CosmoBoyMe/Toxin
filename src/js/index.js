2f1c4e1b20d1ef8e91c919dbb28ac38b8c0a503f
CosmoboyMe <2014failz2014@gmail.com>
2022-02-06 05:48:47 +0100

    feat: add header

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,7 @@
 import '../styles/index.scss';
+
+const burger = document.querySelector('.header__nav-burger');
+
+burger.addEventListener('click', (e) => {
+  e.currentTarget.classList.toggle('header__nav-burger--active');
+});
