84e52da15ff28e3f06c0523c76694f6bf65051dc
CosmoboyMe <2014failz2014@gmail.com>
2022-02-16 21:28:58 +0100

    feat: add dropdown component

--- src/pages/landingPage/landingPage.js
+++ src/pages/landingPage/landingPage.js
@@ -1,2 +1,7 @@
 import './landingPage.scss';
 import '../../components/calender/calender.js';
+import { initDropdown } from '../../components/dropdown/dropdown';
+
+const wordsDeclensions = [['гость', 'гостя', 'гостей']];
+
+initDropdown('guestDropdown', wordsDeclensions);
