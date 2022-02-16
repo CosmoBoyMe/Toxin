4e03446764039955646f4b1dd676af8c0b2b23e7
CosmoboyMe <2014failz2014@gmail.com>
2022-02-16 23:33:39 +0100

    feat: create room details page

---
+++ src/pages/roomDetails/roomDetails.js
@@ -0,0 +1,7 @@
+import '../../styles/index.scss';
+import './roomDetails.scss';
+import '../../components/bookingForm/bookingForm';
+
+import { initPieChart } from '../../components/pieChart/pieChart';
+
+initPieChart('chart', [0, 65, 65, 130]);
