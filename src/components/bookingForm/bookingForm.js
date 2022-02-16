86d598d94fe12b2a315248ed245ec2a62e4025ba
CosmoboyMe <2014failz2014@gmail.com>
2022-02-16 23:27:52 +0100

    feat: create booking form component

---
+++ src/components/bookingForm/bookingForm.js
@@ -0,0 +1,6 @@
+import '../calender/calender.js';
+import { initDropdown } from '../dropdown/dropdown.js';
+
+const wordsDeclensions = [['гость', 'гостя', 'гостей']];
+
+initDropdown('guestDropdown', wordsDeclensions);
