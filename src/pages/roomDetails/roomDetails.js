8d178300d6fdbd9f47ffe840dd65a3df43d31b29
CosmoboyMe <2014failz2014@gmail.com>
2022-02-17 03:01:03 +0100

    feat: create responsive design for room details page

--- src/pages/roomDetails/roomDetails.js
+++ src/pages/roomDetails/roomDetails.js
@@ -1,5 +1,6 @@
 import '../../styles/index.scss';
 import './roomDetails.scss';
+import '../../components/likeBtn/likeBtn.js';
 import '../../components/bookingForm/bookingForm';
 
 import { initPieChart } from '../../components/pieChart/pieChart';
