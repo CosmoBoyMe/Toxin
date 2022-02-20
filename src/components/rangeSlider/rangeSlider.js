827e3aeb0881281e33d8791604dfd894ac4efd08
CosmoboyMe <2014failz2014@gmail.com>
2022-02-20 22:24:06 +0100

    feat: create range slider component

---
+++ src/components/rangeSlider/rangeSlider.js
@@ -0,0 +1,41 @@
+import noUiSlider from 'nouislider';
+import wNumb from 'wnumb';
+
+// ARGUMENTS
+// id: string
+// start: [number, number]
+// range: {min: number, max: number}
+// step: number
+
+const initRangeSlider = (
+  id,
+  start = [5000, 10000],
+  range = {
+    min: 1000,
+    max: 15500,
+  },
+  step = 100,
+) => {
+  const sliderContainerEl = document.getElementById(id);
+  const rangeSliderEl = sliderContainerEl.querySelector('.range-slider__slider');
+  noUiSlider.create(rangeSliderEl, {
+    start,
+    connect: true,
+    step,
+    range,
+
+    format: wNumb({
+      decimals: 0,
+      thousand: ' ',
+      suffix: '₽',
+    }),
+  });
+
+  const rangeSliderValueEl = sliderContainerEl.querySelector('.range-slider__price');
+  rangeSliderEl.noUiSlider.on('update', (values, handle) => {
+    const valuesString = values.join(' - ');
+    rangeSliderValueEl.textContent = valuesString;
+  });
+};
+
+export { initRangeSlider };
