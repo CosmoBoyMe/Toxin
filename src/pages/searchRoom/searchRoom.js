df1805bd68eec93bf589576000e105d6b2c6707e
CosmoboyMe <2014failz2014@gmail.com>
2022-02-26 18:22:00 +0100

    feat: add paginator to search room page

--- src/pages/searchRoom/searchRoom.js
+++ src/pages/searchRoom/searchRoom.js
@@ -2,3 +2,6 @@ import '../../styles/index.scss';
 import './searchRoom.scss';
 import '../../components/filter/filter';
 import '../../components/roomCard/roomCard';
+import { initPagination } from '../../components/pagination/pagination';
+
+initPagination(169, 12);
