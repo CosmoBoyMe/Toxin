d00b3b426656c51a9b66f7dc872c12bc82fe42be
CosmoboyMe <2014failz2014@gmail.com>
2022-03-20 23:29:37 +0100

    refactor: add booking form

--- src/pages/roomDetails/roomDetails.js
+++ src/pages/roomDetails/roomDetails.js
@@ -3,8 +3,9 @@ import './roomDetails.scss';
 
 import '../../components/header/header';
 import '../../components/likeBtn/likeBtn.js';
-import '../../components/bookingForm/bookingForm';
+import { initBookingForm } from '../../components/bookingForm/bookingForm';
 
 import { initPieChart } from '../../components/pieChart/pieChart';
 
 initPieChart('chart', [0, 65, 65, 130]);
+initBookingForm();
