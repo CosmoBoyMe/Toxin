4bb732aebad88a5ca03ccefdd1a129a4eb0f69b5
CosmoboyMe <2014failz2014@gmail.com>
2022-11-05 02:59:24 +0100

    refactor: burger menu style on mobile #10

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,12 +2,42 @@ class Header {
   constructor(element) {
     this.element = element;
     this.init();
+    this.isBurgerMenuActive = false;
+  }
+
+  toggleBodyScroll = () => {
+    if (this.bodyElement.offsetWidth <= 768) {
+      this.bodyElement.classList.add('body_scroll_disable');
+    } else {
+      this.bodyElement.classList.remove('body_scroll_disable');
+    }
+  }
+
+  handleDocumentClick = (event) => {
+    const { headerNavElement } = this;
+    if (!headerNavElement.contains(event.target) ) {
+      this.toggleBurgerMenu();
+    }
+  };
+
+  toggleBurgerMenu = () => {
+    this.isBurgerMenuActive  = !this.isBurgerMenuActive;
+    const { burgerElement, headerNavElement } = this;
+    burgerElement.classList.toggle('header__nav-burger_active');
+    headerNavElement.classList.toggle('header__nav_active');
+    if (this.isBurgerMenuActive) {
+      this.toggleBodyScroll();
+      document.addEventListener('click', this.handleDocumentClick);
+      window.addEventListener('resize', this.toggleBodyScroll);
+    } else {
+      document.removeEventListener('click', this.handleDocumentClick);
+      window.removeEventListener('resize', this.toggleBodyScroll);
+      this.bodyElement.classList.remove('body_scroll_disable')
+    }
   }
 
   handleNavBurgerClick = () => {
-    const { burgerElement, contentElement } = this;
-    burgerElement.classList.toggle('header__nav-burger_active');
-    contentElement.classList.toggle('header__nav-content_active');
+    this.toggleBurgerMenu()
   };
 
   bindBurgerListener() {
@@ -16,7 +46,8 @@ class Header {
 
   init() {
     this.burgerElement = this.element.querySelector('.js-header__nav-burger');
-    this.contentElement = this.element.querySelector('.js-header__nav-content');
+    this.headerNavElement = this.element.querySelector('.js-header__nav');
+    this.bodyElement = document.querySelector('body');
     this.bindBurgerListener();
   }
 }
